382f75145892c3ec2623ed3bb0b0aa6f7910a54a
FawazAshrafAli <fawazashrafali@gmail.com>
2025-01-25 23:45:00 +0530

    Updated template and views of home app

--- bzindia/home/views.py
+++ bzindia/home/views.py
@@ -3,7 +3,10 @@ from django.views.generic import TemplateView, View, ListView
 import logging
 
 from company.models import Company
-from product.models import Product
+from product.models import Product, Category as ProductCategory
+from service.models import Service, Category as ServiceCategory
+from registration.models import RegistrationType
+from educational.models import Program
 
 logger = logging.getLogger(__name__)
 
@@ -24,7 +27,12 @@ class HomeView(BaseHomeView, TemplateView):
         context = super().get_context_data(**kwargs)
         try:
             context["companies"] = Company.objects.all().order_by("name")
-            context["products"] = Product.objects.all().order_by("name")
+            context["product_categories"] = ProductCategory.objects.all().order_by("name")
+            context["service_categories"] = ServiceCategory.objects.all().order_by("name")
+            context["course_programs"] = Program.objects.all().order_by("name")
+            context["registration_types"] = RegistrationType.objects.all().order_by("name")
+            context["services"] = Service.objects.all()
+            context["products"] = Product.objects.all()
         except Exception as e:
             logger.error(f"Error in fetching the context data of home view: {e}")
         return context
