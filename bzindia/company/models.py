0ac2d54c1f8a2c1b02b0232eaf9b3030403db22c
FawazAshrafAli <fawazashrafali@gmail.com>
2025-02-11 09:46:18 +0530

    Created new models

--- bzindia/company/models.py
+++ bzindia/company/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from locations.models import UniquePlace
 
 class CompanyType(models.Model):
     name = models.CharField(max_length=150)
@@ -56,3 +57,136 @@ class Company(models.Model):
         db_table = "company"
         ordering = ["name"]
 
+
+class Client(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=250)
+    image = models.ImageField(upload_to="clients/")
+    order = models.PositiveIntegerField(default=0)
+
+    slug = models.SlugField(null=True, blank=True, max_length=500)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(f"{self.company.name}-{self.name}")
+
+            slug = base_slug
+            count = 1
+
+            while Client.objects.filter(slug = slug).exists():
+                slug = f"{base_slug}-{count}"
+                count += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.company.name}-{self.name}"
+
+    class Meta:
+        db_table = "clients"
+        ordering = ["name"]
+
+
+class Testimonial(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="testimonial_company")
+
+    name = models.CharField(max_length=250)
+    image = models.ImageField(upload_to="testimonials/")
+
+    client_company = models.CharField(max_length=250)
+    place = models.ForeignKey(UniquePlace, on_delete=models.CASCADE, related_name="testimonial_place")
+
+    text = models.TextField()
+    rating = models.PositiveIntegerField(default=5)
+
+    order = models.PositiveIntegerField(default=0)
+
+    slug = models.SlugField(null=True, blank=True, max_length=500)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(f"{self.company.name}-{self.name}-{self.client_company}-{self.place.name}")
+
+            slug = base_slug
+            count = 1
+
+            while Testimonial.objects.filter(slug = slug).exists():
+                slug = f"{base_slug}-{count}"
+                count += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.company.name}-{self.name}-{self.client_company}-{self.place.name}"
+
+    class Meta:
+        db_table = "testimonials"
+        ordering = ["created"]
+
+    @property
+    def get_image_name(self):
+        if self.image:
+            return f"{self.image.name}".replace('testimonials/', '')
+        return None
+    
+
+class MultiPage(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="multi_page_company")
+
+    text = models.TextField()
+
+    slug = models.SlugField(null=True, blank=True, max_length=500)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.company.name)
+
+            slug = base_slug
+            count = 1
+
+            while MultiPage.objects.filter(slug = slug).exists():
+                slug = f"{base_slug}-{count}"
+                count += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.company.name
+
+    class Meta:
+        db_table = "multipages"
+        ordering = ["created"]
+
+    @property
+    def get_content(self):
+        if self.text:
+            content = self.text
+
+            replacing_data = {
+                'state_name': "Kerala",
+                'district_name': "Malappuram",
+                "block_name": "Perinthalmanna",                
+            }
+
+            for key, value in replacing_data.items():
+                content = content.replace(key, value)
+            
+            return content
+        
+        return None
