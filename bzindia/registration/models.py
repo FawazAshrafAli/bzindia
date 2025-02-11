e60d195e4040caa13cbc7be07f72b303f683f3c3
FawazAshrafAli <fawazashrafali@gmail.com>
2025-02-11 09:43:32 +0530

    Created new models

--- bzindia/registration/models.py
+++ bzindia/registration/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.text import slugify
 
 from company.models import Company
+from locations.models import UniqueState
 
 class RegistrationType(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
@@ -103,4 +104,75 @@ class RegistrationDetail(models.Model):
     
     class Meta:
         db_table = "registration_detail"
+        ordering = ["-created"]
+
+
+class Faq(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="registration_faq_company")
+    registration_sub_type = models.ForeignKey(RegistrationSubType, on_delete=models.CASCADE)
+
+    question = models.TextField()
+    answer = models.TextField()
+
+    slug = models.SlugField(null=True, blank=True, max_length=500)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(f"{self.company.name}-{self.registration_sub_type.name}")
+            slug = base_slug
+
+            count = 1
+            while Faq.objects.filter(slug = slug).exists():
+                slug = f"{base_slug}-{count}"
+                count += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.company.name}-{self.registration_sub_type.name}"
+    
+    class Meta:
+        db_table = "registration_faqs"
+        ordering = ["-created"]
+
+
+class Enquiry(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="registration_enquiry_company")
+
+    name = models.CharField(max_length=250)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+    registration_sub_type = models.ForeignKey(RegistrationSubType, on_delete=models.CASCADE)
+    state = models.ForeignKey(UniqueState, on_delete=models.CASCADE, related_name="registration_enquiry_state")
+    message = models.TextField()
+
+    slug = models.SlugField(null=True, blank=True, max_length=500)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(f"{self.company.name}-{self.registration_sub_type.name}-{self.email}")
+            slug = base_slug
+
+            count = 1
+            while Enquiry.objects.filter(slug = slug).exists():
+                slug = f"{base_slug}-{count}"
+                count += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.company.name}-{self.registration_sub_type.name}-{self.email}"
+    
+    class Meta:
+        db_table = "registration_enquiries"
         ordering = ["-created"]
