d131499f75ee7335321c5058a8554fa79a0b8949
FawazAshrafAli <fawazashrafali@gmail.com>
2025-03-22 14:21:10 +0530

    Updated Project (22-03-2025)

--- bzindia/base/views.py
+++ bzindia/base/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+from company.models import Company, CompanyType
+from blog.models import Blog
+
+class BaseView(View):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["company_types"] = CompanyType.objects.all().order_by("name")
+        context["slider_blogs"] = Blog.objects.all().order_by("?")[:3]
+
+
+        return context
---
+++ bzindia/blog/models.py
@@ -0,0 +1,105 @@
+from django.db import models
+from django.utils.text import slugify
+from django.utils import timezone
+
+from educational.models import Course
+from product.models import Product
+from service.models import Service
+from registration.models import RegistrationSubType
+from company.models import Company
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    image = models.ImageField(upload_to="blogs/", null=True, blank=True)
+
+    blog_type = models.CharField(max_length=150)
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, null=True, blank=True)
+    registration_sub_type = models.ForeignKey(RegistrationSubType, on_delete=models.CASCADE, null=True, blank=True)
+
+    summary = models.TextField()
+    content = models.TextField()
+    meta_description = models.TextField()
+    
+    meta_tags = models.CharField(max_length=500)
+
+    is_published = models.BooleanField(default=False)
+
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    slug = models.SlugField(null=True, blank=True, max_length=500)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def save(self, *args, **kwargs):
+        if self.is_published:
+            self.published_date = timezone.now()
+        else:
+            self.published_date = None
+
+        if not self.slug:
+            topic = (
+                getattr(self.course, "name", None) or
+                getattr(self.product, "name", None) or
+                getattr(self.service, "name", None) or
+                getattr(self.registration_sub_type, "name", "")
+            )
+
+            base_slug = slugify(f"{self.title}-{self.type}-{topic}")
+
+            slug = base_slug
+            count = 1
+
+            while Blog.objects.filter(slug = slug).exists():
+                slug = f"{base_slug}-{count}"
+                count += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        db_table = "blogs"
+        ordering = ["-created"]
+
+    @property
+    def get_topic(self):
+        topic = None
+        if self.course:
+            topic = self.course.name
+        elif self.product:
+            topic = self.product.name
+        elif self.registration_sub_type:
+            topic = self.registration_sub_type.name
+        elif self.service:
+            topic = self.service.name
+            
+        return topic
+    
+    @property
+    def get_image_name(self):
+        if self.image:
+            return self.image.name.replace("blogs/", "")
+        
+        return None
+    
+    @property
+    def get_object(self):
+        if self.blog_type == "Education":
+            return self.course.name
+        elif self.blog_type == "Product":
+            return self.product.name
+        elif self.blog_type == "Service":
+            return self.service.name
+        elif self.blog_type == "Registration":
+            return self.registration_sub_type.name
+        else:
+            return None
